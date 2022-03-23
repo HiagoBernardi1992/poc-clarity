207b5ad2bf2c868c7d08cdbd6529716ee80dc79c
Hiago Bernardi <hiago.bernardi@gmail.com>
2022-03-23 11:59:38 +0000

    finished we can control the state of the table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './model/user.model';
+import { ClrDatagridStateInterface } from '@clr/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  users : IUser[] = [
+    { id: 1, name: "user 1", creation: new Date, color: "Red" },
+    { id: 2, name: "user 2", creation: new Date, color: "Red" },
+    { id: 3, name: "user 3", creation: new Date, color: "Red" },
+    { id: 4, name: "user 4", creation: new Date, color: "Red" },
+    { id: 5, name: "user 5", creation: new Date, color: "Red" },
+    { id: 6, name: "user 6", creation: new Date, color: "Red" },
+    { id: 7, name: "user 7", creation: new Date, color: "Red" },
+    { id: 8, name: "user 8", creation: new Date, color: "Red" },
+    { id: 9, name: "user 9", creation: new Date, color: "Red" },
+    { id: 10, name: "user 10", creation: new Date, color: "Red" },
+  ];
+
+  refresh(state: ClrDatagridStateInterface){
+    console.log('state', state);
+  }
 }
---
+++ src/app/model/user.model.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+    id: number,
+    name: string,
+    color: string,
+    creation: Date,
+}
